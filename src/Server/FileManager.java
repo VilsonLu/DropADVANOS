50eccfb3f660edfb21ee8bff74930a9e98b1ee17
givmefive5 <givme_five5@Yahoo.com>
2014-11-26 12:18:28 +0800

    finished but untested

--- src/Server/FileManager.java
+++ src/Server/FileManager.java
@@ -8,14 +8,17 @@ public class FileManager {
 
 	public static final String folderLocation = "Server/";
 
-	private HashMap<String, Monitor> fileMap = new HashMap<>();
+	private static HashMap<String, Monitor> fileMap = new HashMap<>();
 
 	// will load all files from Server folder and assign each a monitor for
 	// mutex.
 	public static void initializeFileLocks() {
-		// scan for existing files in folder Server, instantiate one monitor for
-		// each
-		// add them to filemap
+		File folder = new File(folderLocation);
+		File[] listOfFiles = folder.listFiles();
+		for (File f : listOfFiles) {
+			String filename = f.getName();
+			fileMap.put(filename, new Monitor());
+		}
 	}
 
 	public static boolean clientHasALaterCopy(String filename, Timestamp t) {
@@ -27,12 +30,23 @@ public class FileManager {
 	}
 
 	public static synchronized void acquireLockOfFile(String filename) {
+
+		if (fileMap.get(filename) == null)
+			fileMap.put(filename, new Monitor());
+
+		Monitor m = fileMap.get(filename);
+		m.waiting();
+
 		// if filename in keys, get monitor and lock
 		// else create new monitor, put to fileMap
 		// acquirelock
 	}
 
 	public static synchronized void releaseLockOfFile(String name) {
+
+		Monitor m = fileMap.get(name);
+		m.signaling();
+
 		// get monitor of file from filemap
 		// release lock
 	}
